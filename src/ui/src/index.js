2a570b33446530baa151901112230f025f6cdfe5
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-09 07:10:39 +0100

    Inject profile service

--- src/ui/src/index.js
+++ src/ui/src/index.js
@@ -1,6 +1,7 @@
 import {inject} from 'aurelia-framework'
 import {ProfileService} from 'profile/profile-service'
 
+@inject(ProfileService)
 export class Index {
   constructor(profileService){
     profileService.getProfile().then((p) => this.profile = p);
