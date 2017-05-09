54c6dcd9b11b48f45e38922940e35d9bc78c3061
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-09 06:38:39 +0100

    Add markup

--- src/ui/src/index.js
+++ src/ui/src/index.js
@@ -1,3 +1,8 @@
+import {inject} from 'aurelia-framework'
+import {ProfileService} from 'profile/profile-service'
+
 export class Index {
-  
+  constructor(profileService){
+    profileService.getProfile().then((p) => this.profile = p);
+  }
 }
--- src/ui/src/plugin.js
+++ src/ui/src/plugin.js
@@ -1,4 +1,6 @@
 export function configure(config){
   let container = config.container;
-  container.registerInstance('plugin.route',  { route: ['profile'],       name: 'profile',       moduleId: 'profile/index' })
+  //container.registerInstance('plugin.route',  { route: ['profile'],       name: 'profile',       moduleId: 'profile/index' })
+
+  container.registerInstance('plugin.widget.homepage.component', {viewModel: 'profile/index', view:'profile/index.html'});
 }
