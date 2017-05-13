3cf878c0b9879307aa59f36f8dbaf6661247f6e2
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-13 21:27:36 +0100

    add admin

--- src/ui/src/profile/plugin.js
+++ src/ui/src/profile/plugin.js
@@ -1,6 +1,6 @@
 export function configure(config){
   let container = config.container;
-  container.registerInstance('plugin.admin.route',  { route: ['admin/profile'],  name: 'edit-profile',  moduleId: 'profile/admin/edit', title: 'Profile' })
+  container.registerInstance('plugin.admin.route',  { route: ['profile'],  name: 'edit-profile',  moduleId: 'profile/admin/edit', title: 'Profile' })
 
   container.registerInstance('plugin.widget.homepage.component', {
     title: 'Profile',
