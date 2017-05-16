9bf60ef6d8e3cd7e1ad48e613302f6996b5812ae
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-16 21:51:35 +0100

    Add save profile

--- src/ui/src/profile/admin/edit.js
+++ src/ui/src/profile/admin/edit.js
@@ -1,3 +1,16 @@
-export class Edit {
+import {inject} from 'aurelia-framework'
+import {ProfileService} from './../profile-service'
 
+
+@inject(ProfileService)
+export class Edit {
+  constructor(profileService){
+    this.profileService = profileService;
+  }
+  activate() {
+    this.profileService.getProfile().then(p => this.profile = p)
+  }
+  save(profile){
+    this.profileService.saveProfile(profile);
+  }
 }
--- src/ui/src/profile/profile-service.js
+++ src/ui/src/profile/profile-service.js
@@ -9,6 +9,15 @@ function deserialize(item){
   return retval;
 }
 
+function serialize(item){
+  let retval = {};
+  for(let key of Object.keys(item)){
+    var value = AWS.DynamoDB.Converter.input(item[key]);
+    retval[key] = value;
+  }
+  return retval;
+}
+
 export class ProfileService {
   getProfile() {
     return new Promise((resolve, reject) => {
@@ -19,6 +28,19 @@ export class ProfileService {
           if (err) reject(err); // an error occurred
           else    {
             resolve(deserialize(data.Items[0]));
+          }
+      });
+    });
+  }
+  saveProfile(profile) {
+    return new Promise((resolve, reject) => {
+
+      var table = new AWS.DynamoDB({apiVersion: '2012-08-10', params: {TableName: 'cv_overkill_profile'}});
+
+      return table.putItem({Item: serialize(profile)}, function(err, data) {
+          if (err) reject(err); // an error occurred
+          else    {
+            resolve();
           }
       });
     });
